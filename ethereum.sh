9d0fdd2eca2d6e7f12af0e9a785b63ce511b6f60
lightswarm124 <lightswarm124@users.noreply.github.com>
2016-09-12 15:01:03 -0400

    Add files via upload

---
+++ ethereum.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo add-apt-repository ppa:ethereum/ethereum-qt
+sudo add-apt-repository ppa:ethereum/ethereum
+sudo add-apt-repository ppa:ethereum/ethereum-dev
+sudo apt-get update
+sudo apt-get install -y ethereum solc nodejs npm
+
